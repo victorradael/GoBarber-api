e24f7c6d25ab7decd3578e0f08b5e3a70d688236
Victor Radael <victorradael@hotmail.com>
2020-05-31 23:38:15 -0300

    :rocket: Button and Ipntu on the SignIn screen

--- mobilegobarber/src/pages/SignIn/index.tsx
+++ mobilegobarber/src/pages/SignIn/index.tsx
@@ -4,11 +4,24 @@ import { Image } from 'react-native';
 import { Container, Title } from './styles';
 import logoImg from '../../assets/logo.png';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu login</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('Button Press');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
