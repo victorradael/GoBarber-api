9e1fbf02cc33d8c371e1d28ff5d2356cafc169a6
Victor Radael <victorradael@hotmail.com>
2020-07-24 22:25:26 -0300

    :rocket: Add Provider Controllers Appointments and Availability

--- api_gobarber/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ api_gobarber/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 
 import AppointmentController from '@modules/appointments/infra/http/controllers/AppointmentsController';
+import ProviderAppointmentsController from '@modules/appointments/infra/http/controllers/ProviderAppointmentsController';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
 const appointmentController = new AppointmentController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.post('/', appointmentController.create);
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
---
+++ api_gobarber/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -0,0 +1,36 @@
+import { injectable, inject } from 'tsyringe';
+
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  day: number;
+  year: number;
+}
+
+@injectable()
+export default class ListProviderAppointmentsService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository
+  ) {}
+  public async execute({
+    provider_id,
+    month,
+    year,
+    day,
+  }: IRequest): Promise<Appointment[]> {
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        day,
+        month,
+        provider_id,
+        year,
+      }
+    );
+
+    return appointments;
+  }
+}
