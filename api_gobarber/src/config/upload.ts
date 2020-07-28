41a8338e935b3393a223eb1abb0dad6a4cd27f2e
Victor Radael <victorradael@hotmail.com>
2020-07-28 15:33:07 -0300

    :rocket: Amazon S3

--- api_gobarber/src/config/upload.ts
+++ api_gobarber/src/config/upload.ts
@@ -1,20 +1,48 @@
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import crypto from 'crypto';
 import path from 'path';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
+interface IUploadConfig {
+  driver: 's3' | 'diskStorage';
+
+  tmpFolder: string;
+  uploadsFolder: string;
+
+  multer: {
+    storage: StorageEngine;
+  };
+
+  config: {
+    diskStorage: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER,
   tmpFolder: tmpFolder,
   uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('HEX');
-      const fileName = `${fileHash}-${file.originalname}`;
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('HEX');
+        const fileName = `${fileHash}-${file.originalname}`;
 
-      return callback(null, fileName);
+        return callback(null, fileName);
+      },
+    }),
+  },
+
+  config: {
+    diskStorage: {},
+    aws: {
+      bucket: 'app-trustnavigation',
     },
-  }),
-};
+  },
+} as IUploadConfig;
